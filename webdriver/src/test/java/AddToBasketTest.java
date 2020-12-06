ce06e58ab79ee227bc545d517f8583294752e43d
KuzyaCat <lizerd34@mail.ru>
2020-12-06 22:20:24 +0300

    Add fluent wait

--- webdriver/src/test/java/AddToBasketTest.java
+++ webdriver/src/test/java/AddToBasketTest.java
@@ -1,20 +1,23 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class AddToBasketTest {
     private final String CHROME_DRIVER_PATH = "/drivers/chromedriver";
-//    private final String PAGE_URL = "https://cutt.ly/IhmH8m3";
-    private final String PAGE_URL = "https://deal.by/p115977873-naruchnye-chasy-amst.html?_openstat=by_prosale%3B%D1%87%D0%B0%D1%81%D1%8B+%D0%BD%D0%B0%D1%80%D1%83%D1%87%D0%BD%D1%8B%D0%B5+%D0%B8+%D0%BA%D0%B0%D1%80%D0%BC%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%3B%D0%9D%D0%90%D0%A0%D0%A3%D0%A7%D0%9D%D0%AB%D0%95++%D0%A7%D0%90%D0%A1%D0%AB+AMST%3Bsearch";
+    private final String PAGE_URL = "https://cutt.ly/IhmH8m3"; // short link
+//    private final String PAGE_URL = "https://deal.by/p115977873-naruchnye-chasy-amst.html?_openstat=by_prosale%3B%D1%87%D0%B0%D1%81%D1%8B+%D0%BD%D0%B0%D1%80%D1%83%D1%87%D0%BD%D1%8B%D0%B5+%D0%B8+%D0%BA%D0%B0%D1%80%D0%BC%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%3B%D0%9D%D0%90%D0%A0%D0%A3%D0%A7%D0%9D%D0%AB%D0%95++%D0%A7%D0%90%D0%A1%D0%AB+AMST%3Bsearch";
     private final String BUY_BUTTON_CLASSNAME = "js-product-buy-button";
 
     private final long SECONDS_TO_WAIT = 10;
+    private final long POLLING_SECONDS = 1;
     private WebDriver driver;
     private JavascriptExecutor jsExecutor;
 
@@ -30,10 +33,18 @@ public class AddToBasketTest {
     @Test (description = "Test add to basket")
     public void TestAddProductToBasket() {
         driver.get(PAGE_URL);
-        new WebDriverWait(driver, SECONDS_TO_WAIT)
-                .until(JQueryConditions.jQueryAJAXCompleted());
 
-        WebElement buyButton = driver.findElements(By.className(BUY_BUTTON_CLASSNAME)).get(0);
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(SECONDS_TO_WAIT))
+                .pollingEvery(Duration.ofSeconds(POLLING_SECONDS))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .withMessage("Timeout for waiting was exceeded!");
+
+        WebElement buyButton = wait
+                .until(ExpectedConditions
+                        .presenceOfAllElementsLocatedBy(By.className(BUY_BUTTON_CLASSNAME))).get(0);
+
         System.out.println("buyButton: " + buyButton);
     }
 
