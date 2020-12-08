798ef52029b343e048a917da008c7a7fc716e305
KuzyaCat <lizerd34@mail.ru>
2020-12-08 22:54:25 +0300

    Add DealByWristWatchPage

--- page-object/src/test/java/page/DealByWristWatchPage.java
+++ page-object/src/test/java/page/DealByWristWatchPage.java
@@ -1,12 +1,36 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class DealByWristWatchPage extends Page {
     private final String PAGE_URL = "https://cutt.ly/IhmH8m3"; // short link
 
+    @FindBy(className = "js-product-buy-button")
+    private WebElement buyButton;
+
+    @FindBy(className = "x-header__controls-counter")
+    private WebElement basketItemsCounter;
+
+    @FindBy(className = "x-shc-item__title-link")
+    private WebElement itemTitle;
+
     public DealByWristWatchPage(WebDriver driver) {
         super(driver);
+    }
+
+    public DealByWristWatchPage clickBuyButton() {
+        buyButton.click();
+        return this;
+    }
+
+    public int getBasketItemsCounterValue() {
+        return Integer.parseInt(basketItemsCounter.getText());
+    }
+
+    public String getItemTitleText() {
+        return itemTitle.getText();
     }
 
     public DealByWristWatchPage openPage() {
