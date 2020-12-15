83b50b34fc6872733705eeef7356a391ceead1c8
KuzyaCat <lizerd34@mail.ru>
2020-12-15 11:58:13 +0300

    Add test listener

--- framework/src/test/java/page/Page.java
+++ framework/src/test/java/page/Page.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import util.waiter.JSWaiter;
+import util.JSWaiter;
 
 import java.time.Duration;
 import java.util.List;
--- framework/src/test/java/util/JSWaiter.java
+++ framework/src/test/java/util/JSWaiter.java
@@ -1,4 +1,4 @@
-package util.waiter;
+package util;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
